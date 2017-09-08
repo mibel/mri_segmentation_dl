d9dd2968dd9a1e06554199386904278707865cb5
Dave Warren <davideugenewarren@gmail.com>
2017-09-08 16:02:05 -0500

    Added nodes and workflow for bias-correcting FLAIR images.

--- scripts/wmh_processing_wkflow.py
+++ scripts/wmh_processing_wkflow.py
@@ -58,10 +58,19 @@ ants_warp_flair = ni_engine.Node(ni_ants.ApplyTransforms(dimension=3,
                                                           default_value=0),
                                   name='ants_warp_flair')
 
+# Bias correction, T1
 bias_correct_t1_fs_nucorrect = ni_engine.Node(ni_fs.MNIBiasCorrection(),
                                              name='bias_correct_t1_fs_nucorrect')
 bias_correct_t1_ants_n4 = ni_engine.Node(ni_ants.N4BiasFieldCorrection(),
                                         name='bias_correct_t1_ants_n4')
+
+# Bias correction, FLAIR
+bias_correct_flair_fs_nucorrect = ni_engine.Node(ni_fs.MNIBiasCorrection(),
+                                             name='bias_correct_flair_fs_nucorrect')
+bias_correct_flair_ants_n4 = ni_engine.Node(ni_ants.N4BiasFieldCorrection(),
+                                        name='bias_correct_flair_ants_n4')
+
+# Tissue segmentation
 segment_t1_ants_atropos = ni_engine.Node(ni_ants.Atropos(number_of_tissue_classes=3,
                                                         initialization='KMeans',
                                                         save_posteriors=True),
@@ -90,32 +99,13 @@ else:
     sub_list = tuple([str(i) for i in range(145)])
 
 # Infosource
-
 infosource = ni_engine.Node(ni_util.IdentityInterface(fields=['site_top', 'site_bot', 'subject_id']),
                   name="infosource")
 infosource.iterables = [('site_top', site_top_list),
                         ('site_bot', site_bot_list),
                         ('subject_id', sub_list)]
-# infosource = ni_engine.Node(ni_io.DataGrabber(), name='infosource')
 
 # SelectFiles
-
-# t1_file = opj('wmh', '{site_top}', '{site_bot}', '{subject_id}', 'orig', '3DT1.nii.gz')
-# t1_mask = opj('wmh', '{site_top}', '{site_bot}', '{subject_id}', 'orig', '3DT1_mask.nii.gz')
-# t1_file_alt = opj('wmh', '{site_top}', '{site_bot}', '{subject_id}', 'orig', 'T1.nii.gz')
-# t1_alt_mask = opj('wmh', '{site_top}', '{site_bot}', '{subject_id}', 'orig', 'T1_mask.nii.gz')
-# flair_file = opj('wmh', '{site_top}', '{site_bot}',
-#                  '{subject_id}', 'orig', 'FLAIR.nii.gz')
-# lesion_file = opj('wmh', '{site_top}', '{site_bot}',
-#                   '{subject_id}', 'wmh.nii.gz')
-#
-# templates = {'t1': t1_file,
-#              't1_mask': t1_mask,
-#              't1_alt': t1_file_alt,
-#              't1_alt_mask': t1_alt_mask,
-#              'flair': flair_file,
-#              'lesion': lesion_file}
-
 t1_file = opj('wmh', '{site_top}', '{site_bot}', '{subject_id}', 'orig', '3DT1.nii.gz')
 t1_file_alt = opj('wmh', '{site_top}', '{site_bot}', '{subject_id}', 'orig', 'T1.nii.gz')
 flair_file = opj('wmh', '{site_top}', '{site_bot}',
@@ -138,6 +128,7 @@ ds_resubs = [(r'_site_bot_(?P<site_bot>.*)_site_top_(?P<site_top>.*)_subject_id_
               r'sub-\g<site_top>\g<subject_id>'),
              (r'3DT1\.nii\.gz', r'T1w_base.nii.gz'),
              (r'3DT1', r'T1w'),
+             (r'FLAIR_trans', r'FLAIR_warped'),
              (r'_corrected', '_bias_corrected_ants_n4'),
              (r'_output', '_bias_corrected_fs_nucorrect'),
              (r'wmh_trans', r'lesion_mask_t1'),
@@ -165,16 +156,16 @@ preproc.connect([(infosource, selectfiles, [('site_top', 'site_top'),
                  (bet_t1, bias_correct_t1_fs_nucorrect, [('mask_file', 'mask')]),                 
                  (selectfiles, ants_reg, [('t1', 'moving_image'),
                                           ('t1_alt', 'fixed_image')]),
-                 # (selectfiles, ants_reg, [('t1', 'moving_image'),
-                 #                          ('t1_alt', 'fixed_image'),
-                 #                          ('t1_mask', 'moving_image_mask'),
-                 #                          ('t1_alt_mask', 'fixed_image_mask')]),
                  (ants_reg, ants_warp_lesion, [('reverse_transforms', 'transforms')]),
                  (selectfiles, ants_warp_lesion, [('t1', 'reference_image'),
                                            ('lesion', 'input_image')]),
                  (ants_reg, ants_warp_flair, [('reverse_transforms', 'transforms')]),
                  (selectfiles, ants_warp_flair, [('t1', 'reference_image'),
                                            ('flair', 'input_image')]),
+                 (ants_warp_flair, bias_correct_flair_ants_n4, [('output_image', 'input_image')]),
+                 (bet_t1, bias_correct_flair_ants_n4, [('mask_file', 'mask_image')]),                 
+                 (ants_warp_flair, bias_correct_flair_fs_nucorrect, [('output_image', 'in_file')]),
+                 (bet_t1, bias_correct_flair_fs_nucorrect, [('mask_file', 'mask')]),                 
                  (bias_correct_t1_ants_n4, segment_t1_ants_atropos, [('output_image', 'intensity_images')]),
                  (bet_t1, segment_t1_ants_atropos, [('mask_file', 'mask_image')]),                 
                  (bet_t1, datasink, [('out_file', 'preproc'),
@@ -186,6 +177,8 @@ preproc.connect([(infosource, selectfiles, [('site_top', 'site_top'),
                  (bias_correct_t1_ants_n4, datasink, [('output_image', 'preproc.@bc_ants_n4_t1')]),
                  (segment_t1_ants_atropos, datasink, [('classified_image', 'preproc.@seg_ants_atropos_t1')]),
                  (bias_correct_t1_fs_nucorrect, datasink, [('out_file', 'preproc.@bc_nucorrect_t1')]),
+                 (bias_correct_flair_ants_n4, datasink, [('output_image', 'preproc.@bc_ants_n4_flair')]),
+                 (bias_correct_flair_fs_nucorrect, datasink, [('out_file', 'preproc.@bc_nucorrect_flair')]),
                  (copy_t1, datasink, [('out_file', 'preproc.@t1_base')]),
                  ])
 
